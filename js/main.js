da989a67d48ffa00666d5abe699d02b6e5917277
Aliyah Cottle <aliyahrcottle@gmail.com>
2022-04-19 13:44:51 -0400

    proper js of scrolling type

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+function sendEmail(){
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "amiacoderyet@gmail.com",
+        Password: "Iwtcpf4y!",
+        To: 'amiacoderyet@gmail.com',
+        From: "aliyahrcottle@gmail.com",
+        Subject: "Sending Email using javascript",
+        Body: "Well that was easy!!",
+    });
+}
+
 $(document).ready(function() {
     var containers = $('.container');
 
@@ -89,6 +101,103 @@ $(document).ready(function() {
         })
     }
 });
+
+
+
+$(document).ready(function() {
+    var containers1 = $('.container1');
+
+    if (containers1.length) {
+        containers1.each(function() {
+            var container1 = $(this);
+
+            // Support small text - copy to fill screen width
+            if (container1.find('.scrolling-text1').outerWidth() < $(window).width()) {
+                var windowToScrolltextRatio1 = Math.round($(window).width() / container1.find('.scrolling-text1').outerWidth()),
+                    scrollTextContent1 = container1.find('.scrolling-text1 .scrolling-text-content1').text(),
+                    newScrollText1 = '';
+                for (var i = 0; i < windowToScrolltextRatio1; i++) {
+                    newScrollText1 += ' ' + scrollTextContent1;
+                }
+                container1.find('.scrolling-text1 .scrolling-text-content1').text(newScrollText1);
+            }
+
+            // Init variables and config
+            var scrollingText1 = container1.find('.scrolling-text1'),
+                scrollingTextWidth1 = scrollingText1.outerWidth(),
+                scrollingTextHeight1 = scrollingText1.outerHeight(true),
+                startLetterIndent1 = parseInt(scrollingText1.find('.scrolling-text-content1').css('font-size'), 10) / 4.8,
+                startLetterIndent1 = Math.round(startLetterIndent1),
+                scrollAmountBoundary1 = Math.abs($(window).width() - scrollingTextWidth1),
+                transformAmount1 = 0,
+                leftBound1 = 0,
+                rightBound1 = scrollAmountBoundary1,
+                transformDirection1 = container1.hasClass('left-to-right') ? -1 : 1,
+                transformSpeed1 = 200;
+
+            // Read transform speed
+            if (container1.attr('speed')) {
+                transformSpeed1 = container1.attr('speed');
+            }
+        
+            // Make scrolling text copy for scrolling infinity
+            container1.append(scrollingText1.clone().addClass('scrolling-text-copy1'));
+            container1.find('.scrolling-text1').css({'position': 'absolute', 'left': 0});
+            container1.css('height', scrollingTextHeight1);
+        
+            var getActiveScrollingText1 = function(direction1) {
+                var firstScrollingText1 = container.find('.scrolling-text1:nth-child(1)');
+                var secondScrollingText1 = container.find('.scrolling-text1:nth-child(2)');
+        
+                var firstScrollingTextLeft1 = parseInt(container.find('.scrolling-text1:nth-child(1)').css("left"), 10);
+                var secondScrollingTextLeft1 = parseInt(container.find('.scrolling-text1:nth-child(2)').css("left"), 10);
+        
+                if (direction1 === 'left') {
+                    return firstScrollingTextLeft1 < secondScrollingTextLeft1 ? secondScrollingText1 : firstScrollingText1;
+                } else if (direction1 === 'right') {
+                    return firstScrollingTextLeft1 > secondScrollingTextLeft1 ? secondScrollingText1 : firstScrollingText1;
+                }
+            }
+        
+            $(window).on('wheel', function(e) {
+                var delta = e.originalEvent.deltaY;
+                
+                if (delta > 0) {
+                    // going down
+                    transformAmount1 += transformSpeed1 * transformDirection1;
+                    container1.find('.scrolling-text1 .scrolling-text-content1').css('transform', 'skewX(10deg)');
+                }
+                else {
+                    transformAmount1 -= transformSpeed1 * transformDirection1;
+                    container1.find('.scrolling-text1 .scrolling-text-content1').css('transform', 'skewX(-10deg)');
+                }
+                setTimeout(function(){
+                    container1.find('.scrolling-text1').css('transform', 'translate3d('+ transformAmount1 * -1 +'px, 0, 0)');
+                }, 10);
+                setTimeout(function() {
+                    container1.find('.scrolling-text1 .scrolling-text-content1').css('transform', 'skewX(0)');
+                }, 500)
+        
+                // Boundaries
+                if (transformAmount1 < leftBound1) {
+                    var activeText1 = getActiveScrollingText1('left');
+                    activeText1.css({'left': Math.round(leftBound1 - scrollingTextWidth1 - startLetterIndent1) + 'px'});
+                    leftBound1 = parseInt(activeText.css("left"), 10);
+                    rightBound1 = leftBound1 + scrollingTextWidth1 + scrollAmountBoundary1 + startLetterIndent1;
+        
+                } else if (transformAmount1 > rightBound1) {
+                    var activeText1 = getActiveScrollingText1('right');
+                    activeText1.css({'left': Math.round(rightBound1 + scrollingTextWidth1 - scrollAmountBoundary1 + startLetterIndent1) + 'px'});
+                    rightBound1 += scrollingTextWidth1 + startLetterIndent1;
+                    leftBound1 = rightBound1 - scrollingTextWidth1 - scrollAmountBoundary1 - startLetterIndent1;
+                }
+            });
+        })
+    }
+});
+
+
+
 
 let options = {
 	root: null,
@@ -193,15 +302,6 @@ $(function () {
 		}
 	};
 
-ScrollReveal().reveal('#loader',{
-	useDelay:'onload'
-});
-
-ScrollReveal().reveal('#top', settings);
-ScrollReveal().reveal('section#explore', settings);
-ScrollReveal().reveal('section#about', settings);
-ScrollReveal().reveal('section#submit', settings);
-ScrollReveal().reveal('section#social', settings);
 
 
 
