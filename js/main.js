c476acf87f855ac392bb04ed4ae3a3bb58b06c44
Aliyah Cottle <aliyahrcottle@gmail.com>
2022-05-13 05:35:00 -0400

    header scroll offset

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
-
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+  && location.hostname == this.hostname) {
+  
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top - 105 //offsets for fixed header
+          }, 1000);
+          return false;
+        }
+      }
+    });
+    //Executed on page load with URL containing an anchor tag.
+    if($(location.href.split("#")[1])) {
+        var target = $('#'+location.href.split("#")[1]);
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top - 125 //offset height of header here too.
+          }, 1000);
+          return false;
+        }
+      }
+  });
 
 document.body.onload = function(){
     setTimeout(function() {
